c87aabd3477a7d77c9f558c34894987899a26f1b
ravindu9701 <47659160+ravindu9701@users.noreply.github.com>
2020-11-02 14:40:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'home.dart';
+
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.code\nError Message: $e.message');
+  }
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RealTime Detection',
+      home: HomePage(cameras),
+    );
+  }
+}
